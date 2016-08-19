fb621a453cd8288dae219e758f9107472cf5d875
albertusadityo <didithere@gmail.com>
2016-08-19 13:19:51 +0700

    added custom crontab and bash file to replace existing crontab

--- main.cpp
+++ main.cpp
@@ -7,8 +7,20 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 
 int main() {
+	std::ofstream myfile ("example.txt");
+
+	std::cout << "Creating the file" << std::endl;
+	if (myfile.is_open())
+	{
+		myfile << "This is a line.\n";
+		myfile << "This is another line.\n";
+		myfile.close();
+	}
+	else
+		std::cout << "Unable to open file";
 
 	return 0;
 }

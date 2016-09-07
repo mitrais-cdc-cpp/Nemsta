64bd4d19cc094c47723233a07f79339e3f432fbf
azisfatoni <azisfatoni@gmail.com>
2016-09-07 09:33:25 +0700

    Tidy up the main function

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ int main(int argc, char *argv[]) {
   NemstaUtil nemsta;
   PDU pdu;
   ReturnStatus status = nemsta.capture(argc, argv, pdu);
-  std::string macAddress = nemsta.getMacAddress();
 
   std::cout << std::endl;
   std::cout << "IP Address  : " << nemsta.getIpAddress() << std::endl;

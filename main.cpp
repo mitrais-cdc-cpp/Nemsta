b099f81cdf9c613fb552568a8e79d9878efac874
suhendra-ep <IKomangSuhendra.EkaPutra@mitrais.com>
2016-09-15 08:55:37 +0800

    Update include header on main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,8 @@
 #include <iostream>
 #include <memory>
 
-#include "env/SNMPDao/inc/DBFactory.hpp"
-#include "env/SNMPDao/inc/DataAccess.hpp"
-#include "env/SNMPDao/inc/Entity/NetworkElement.hpp"
-
 #include "env/SNMPLib/inc/PDU.hpp"
 #include "env/SNMPLib/inc/ReturnStatus.hpp"
-#include "inc/util/DatabaseUtil.hpp"
 #include "inc/util/NemstaUtil.hpp"
 
 using namespace Mitrais::SNMP;
